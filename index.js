6339756ce2f549c79137f4ab7ebf8e71c901a2c3
Ravi kumar <1002kumarravi@gmail.com>
2021-09-26 10:55:53 +0530

    fix- max-date

--- index.js
+++ index.js
@@ -141,6 +141,15 @@ const inputDate = document.querySelector("#input-date")
 const showbtn = document.querySelector(".show-btn")
 const outputMsg = document.querySelector(".output-msg")
 
+let  today = new Date();
+const dd = String(today.getDate()).padStart(2, '0');
+const mm = String(today.getMonth() + 1).padStart(2, '0'); 
+const yyyy = today.getFullYear();
+today = yyyy + '-' + mm + '-' + dd;
+console.log( today)
+
+inputDate.setAttribute("max", `${today}`)
+
 function clickhandler() {
     let bdayStr = inputDate.value
     // console.log(bdayStr)
